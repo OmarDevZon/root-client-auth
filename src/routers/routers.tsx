a71c45d7efd765e62182c2746c501822aa60445f
omar-web-dev-2 <apoloweb2.0@gmail.com>
2024-03-02 01:09:49 +0600

    first commit

---
+++ src/routers/routers.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "../components/layout/MainLayout";
+import { Home } from "../pages/home";
+import { Contact } from "../pages/contact";
+import { Login } from "../pages/login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/utils/navbar.ts
@@ -0,0 +1,14 @@
+export const navbar = [
+  {
+    slug: "/home",
+    name: "Home",
+  },
+  {
+    slug: "/contact",
+    name: "Contact",
+  },
+  {
+    slug: "/login",
+    name: "Login",
+  },
+];
